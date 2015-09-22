5ba71895a5ce89a05b1652c2576484c42b263d9f
Tom Lokhorst <tom@lokhorst.eu>
2015-09-22 13:45:38 +0200

    Added SegueManager.xcodeproj

---
+++ src/SegueManager.h
@@ -0,0 +1,19 @@
+//
+//  SegueManager.h
+//  SegueManager
+//
+//  Created by Tom Lokhorst on 2015-09-22.
+//  Copyright © 2015 nonstrict. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for SegueManager.
+FOUNDATION_EXPORT double SegueManagerVersionNumber;
+
+//! Project version string for SegueManager.
+FOUNDATION_EXPORT const unsigned char SegueManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SegueManager/PublicHeader.h>
+
+
