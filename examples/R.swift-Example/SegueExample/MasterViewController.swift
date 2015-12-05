b56ba7a7d9496b3ed12f2fae1b639e3765ca9fbd
Tom Lokhorst <tom@lokhorst.eu>
2015-12-05 16:55:12 +0100

    Add R.swift example

---
+++ examples/R.swift-Example/SegueExample/MasterViewController.swift
@@ -0,0 +1,33 @@
+//
+//  MasterViewController.swift
+//  SegueExample
+//
+//  Created by Tom Lokhorst on 2015-03-02.
+//  Copyright (c) 2015 Tom Lokhorst. All rights reserved.
+//
+
+import UIKit
+import SegueManager
+
+class MasterViewController: UIViewController {
+
+  var segueManager: SegueManager!
+
+  required init?(coder aDecoder: NSCoder) {
+    super.init(coder: aDecoder)
+
+    // Create a segue manager based on the current view controller
+    segueManager = SegueManager(viewController: self)
+  }
+
+  override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+    segueManager.prepareForSegue(segue)
+  }
+
+  @IBAction func openDetailAction(sender: UIButton) {
+
+    segueManager.performSegue(R.segue.masterViewController.showDetail.identifier) { (detail: DetailViewController) in
+      detail.displayText = "This is the detail screen!"
+    }
+  }
+}
